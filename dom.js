1c9d07854c2d49efa64d9a6224241ed41916382d
MatthewTLackey <lackey.matt@gmail.com>
2013-11-22 18:09:49 -0500

    DOM exercise complete

--- dom.js
+++ dom.js
@@ -10,6 +10,9 @@
 // Find the Google search button and store it in a variable.
 // Modify the text of the button so that it says "Yahooo!" instead.
 
+//var googleLogo = document.getElementById('hplogo');
+//googleLogo.src = "a new url";
+
 // EXERCISE: About Me
 
 // Cuz every webpage needs an "About" section.
@@ -39,6 +42,40 @@
 // Iterate through each li and change the class to "listitem". Add a style tag that sets a rule for "listitem" to make the color red.
 // Create a new img element and set its src attribute to a picture of you. Append that element to the page.
 
+var myVar = document.getElementsByTagName("li");
+var setFont = document.all;
+
+
+var attributes = ["Mateo", "Reese", "Stoneboro", 1, 2, 3, 4, 5, 6]
+
+// var attributes = {
+//   nickname: "Mateo",
+//   favorites: "Reeses Peanut Butter Cups",
+//   hometown: "Stoneboro"
+// };
+
+for(i = 0; i < myVar.length; i++){
+  var temp = myVar[i].getElementsByTagName('span');
+  temp[0].textContent = attributes[i];
+
+}
+
+var pageNode = document.getElementsByTagName('li');
+for(i = 0; i < pageNode.length; i++){
+  pageNode[i].style.color = 'red';
+}
+
+//var image_source = "http://www.womenshealthmag.com/files/wh6_uploads/images/all-you-can-eat-art.jpg";
+
+//document.write("<img src = http://www.womenshealthmag.com/files/wh6_uploads/images/all-you-can-eat-art.jpg />" );
+
+// myVar.nickname = "Matt";
+// myVar.Favorites = "movies";
+// this.hometown = "Stoneboro"
+
+
+
+
 // EXERCISE: The Book List
 
 // Keep track of which books you read and which books you want to read!
@@ -51,5 +88,24 @@
 // Bonus: add a property to each book with the URL of the book cover, and add an img element for each book on the page.
 // Bonus: Change the style of the book depending on whether you have read it or not.
 
+var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe", 
+"John Quincy Adams", "Jackson", "Tyler", "van Buren", "Pierce", "Taylor", "Lincoln", 
+"Johnson", "Grant", "Garfield", "Arthur", "McKinley", "Taft", "Wilson", "Roosevelt", 
+"Roosevelt", "Eisenhower", "Truman", "Kennedy", "Johnson", "Nixon", "Ford", "Carter", 
+"Reagan", "Bush", "Clinton", "Bush", "Obama"];
 
 
+
+var pageNode = document.getElementsByTagName('body')[0];
+
+
+for(i = 0; i < presidents.length; i++){
+  var newLine = document.createElement('p');
+  var newText = document.createTextNode(presidents[i]);
+  //document.write("<p>" + presidents[i] + "</p>");
+  
+  newLine.appendChild(newText);
+  pageNode.appendChild(newLine);
+
+  
+}
